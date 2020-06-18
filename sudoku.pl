039e89c1a14ad9c917b3163a6b1cad53fcb262a6
sadaf <sadaf98x@gmail.com>
2020-06-18 16:37:01 +0430

    start added

--- sudoku.pl
+++ sudoku.pl
@@ -1,5 +1,20 @@
 :- use_module(library(clpfd)).
 
+start:-
+    Puzzle = [[2,_,_,_,9,_,_,3,_],
+              [_,5,_,1,_,_,_,_,_],
+              [9,_,_,_,_,_,6,_,_],
+              [_,_,_,_,5,_,_,_,_],
+              [_,_,_,7,_,_,_,_,8],
+              [_,1,_,_,_,_,_,_,_],
+              [6,_,_,_,_,_,2,_,_],
+              [_,_,_,_,4,_,_,_,_],
+              [_,4,_,_,_,_,7,_,9]],
+    format('###### Sudoku Puzzle ######~n'),
+    sudoku(Puzzle),
+    maplist(label,Puzzle),
+    maplist(portray_clause,Puzzle).
+
 sudoku(Rows):-
     length(Rows, 9),
     maplist(same_length(Rows), Rows),
@@ -22,10 +37,3 @@ squares([N1,N2,N3|Ns1],
         [N7,N8,N9|Ns3]):-
     all_distinct([N1,N2,N3,N4,N5,N6,N7,N8,N9]),
     squares(Ns1,Ns2,Ns3).
-
-
-    
-
-    
-    
-    
